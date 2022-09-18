dae540993907dcdca38a7a5e56c4e22e3f209072
Tharindu Lakmal <tharindu96221@gmail.com>
2022-09-18 15:38:02 +0530

    Updated the Detail page

--- src/MovieGallery/MovieGallery/Controllers/MovieController.cs
+++ src/MovieGallery/MovieGallery/Controllers/MovieController.cs
@@ -10,6 +10,13 @@ namespace MovieGallery.Controllers
     {
         public ActionResult Detail()
         {
+            ViewBag.MovieName = "Zack Snyder's Justice League";
+            ViewBag.Director = "Zack Snyder";
+            ViewBag.Description = "<p><b>Bruce Wayne</b> and <b>Diana Prince</b> try to bring the meta humans of Earth together after the death of <b>Clark Kent</b>. Meanwhile, <b>Darkseid</b> sends <b>Steppenwolf</b> to Earth with a vast army to subjugate humans.</p>";
+            ViewBag.Cast = new string[]
+            {
+                "Henry Cavill", "Ben Afflec", "Gal Gadot", "Jason Mamoa", "Ezra Miller"
+            };
             return View();
         }
     }
